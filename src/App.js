a504cd7c02686a13637bc2fc026a60695facfb11
Brian Ochan <bochan@outbox.co.ug>
2021-03-03 14:10:38 +0300

    Feature: Add data fetching from tretton37 API.
    - Added ability to pull data from the tretton37 API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import './css/main.scss';
 import { FaLinkedin, FaTwitterSquare, FaGithubSquare } from 'react-icons/fa';
+import axios from 'axios';
+
+import { useState, useEffect } from 'react';
+
+const DATA_URL = 'https://api.tretton37.com/ninjas';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(DATA_URL);
+      if (data.status === 200) {
+        setData(data.data);
+        setLoading(false);
+      } else {
+        setErrorMessage(
+          'Something happened while fetching data. Pelease try again later'
+        );
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  // const data = await
   const tretton37Staff = [
     {
       name: 'Brian Ochan',
@@ -55,10 +84,12 @@ const App = () => {
       </div>
       <div className='filters'>Potential filter and tools area</div>
       <div className='main-section'>
-        {tretton37Staff.map((staff) => {
+        {data?.map((staff) => {
           return (
             <div className='card'>
-              <div className='profile-image'>Image</div>
+              <div className='profile-image'>
+                <img src={staff.imagePortraitUrl} alt={staff.name} />
+              </div>
               <div className='profile-details'>
                 <div style={{ alignSelf: 'center' }}>
                   <div className='profile-user-name'>{staff.name}</div>
