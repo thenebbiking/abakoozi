59eaae80ba6ed6cb9047ae43dddb1054daec9af1
Brian Ochan <bochan@outbox.co.ug>
2021-03-03 13:45:36 +0300

    Feature: Setup basic shell.
    - Setup basic app shell with CSS, Layout, Colors and Icons.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/main.scss';
+import { FaLinkedin, FaTwitterSquare, FaGithubSquare } from 'react-icons/fa';
 
-function App() {
+const App = () => {
+  const tretton37Staff = [
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+    {
+      name: 'Brian Ochan',
+      office: 'Lund',
+      linkedInProfile: 'https://linkedIn.com',
+      githubProfile: 'https://linkedIn.com',
+      twitterProfile: 'https://linkedIn.com',
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div style={{ position: 'sticky', top: 0 }}>
+        <header>
+          <p>_the fellowship of the tretton37</p>
+        </header>
+      </div>
+      <div className='filters'>Potential filter and tools area</div>
+      <div className='main-section'>
+        {tretton37Staff.map((staff) => {
+          return (
+            <div className='card'>
+              <div className='profile-image'>Image</div>
+              <div className='profile-details'>
+                <div style={{ alignSelf: 'center' }}>
+                  <div className='profile-user-name'>{staff.name}</div>
+                  <div className='profile-user-office'>{staff.office}</div>
+                </div>
+
+                <div style={{ alignSelf: 'center' }}>
+                  <span>
+                    <FaLinkedin size={20} />
+                  </span>
+                  <span>
+                    <FaGithubSquare />
+                  </span>
+                  <span>
+                    <FaTwitterSquare />
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        {/* 
+
+        
+        */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
